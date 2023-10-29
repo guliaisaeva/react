6cd7ed0602d0e785a7a2572438fa5317d4c98f8b
Gulia <gulia.isaeva.pls@gmail.com>
2023-10-29 17:55:50 +0300

    feat:add cards

--- src/components/FilmCards.tsx
+++ src/components/FilmCards.tsx
@@ -1,27 +1,11 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import { Film } from './types/types';
 
-class FilmCards extends Component {
-  constructor(props: {} | Readonly<{}>) {
-    super(props);
-    this.state = {
-      films: [],
-    };
-  }
+interface FilmCardsProps {
+  films: Film[];
+}
 
-  async componentDidMount() {
-    try {
-      const response = await fetch('https://swapi.dev/api/films/');
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const data = await response.json();
-      this.setState({ films: data.results });
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  }
-
+class FilmCards extends Component<FilmCardsProps> {
   render() {
     const { films } = this.props;
 
@@ -29,68 +13,14 @@ class FilmCards extends Component {
       <div>
         <h1>Star Wars Films</h1>
         <div className="card-container">
-          {films.map(
-            (
-              film: {
-                title:
-                  | string
-                  | number
-                  | boolean
-                  | React.ReactElement<
-                      any,
-                      string | React.JSXElementConstructor<any>
-                    >
-                  | Iterable<React.ReactNode>
-                  | React.ReactPortal
-                  | null
-                  | undefined;
-                director:
-                  | string
-                  | number
-                  | boolean
-                  | React.ReactElement<
-                      any,
-                      string | React.JSXElementConstructor<any>
-                    >
-                  | Iterable<React.ReactNode>
-                  | React.ReactPortal
-                  | null
-                  | undefined;
-                producer:
-                  | string
-                  | number
-                  | boolean
-                  | React.ReactElement<
-                      any,
-                      string | React.JSXElementConstructor<any>
-                    >
-                  | Iterable<React.ReactNode>
-                  | React.ReactPortal
-                  | null
-                  | undefined;
-                release_date:
-                  | string
-                  | number
-                  | boolean
-                  | React.ReactElement<
-                      any,
-                      string | React.JSXElementConstructor<any>
-                    >
-                  | Iterable<React.ReactNode>
-                  | React.ReactPortal
-                  | null
-                  | undefined;
-              },
-              index: React.Key | null | undefined
-            ) => (
-              <div key={index} className="card">
-                <h2>{film.title}</h2>
-                <p>Director: {film.director}</p>
-                <p>Producer: {film.producer}</p>
-                <p>Release Date: {film.release_date}</p>
-              </div>
-            )
-          )}
+          {films.map((film, index) => (
+            <div key={index} className="card">
+              <h2>{film.title}</h2>
+              <p>Director: {film.director}</p>
+              <p>Producer: {film.producer}</p>
+              <p>Release Date: {film.release_date}</p>
+            </div>
+          ))}
         </div>
       </div>
     );

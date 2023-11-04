6ff1175f81719ddcf2ebb5f3416188cda026afb6
Gulia <gulia.isaeva.pls@gmail.com>
2023-11-04 23:51:50 +0300

    refactor:change FilmCard to funnctional component

--- src/components/FilmCards.tsx
+++ src/components/FilmCards.tsx
@@ -1,27 +1,23 @@
-import { Component } from 'react';
+import React from 'react';
 import { Film } from './types/types';
 
 interface FilmCardsProps {
   films: Film[];
 }
 
-class FilmCards extends Component<FilmCardsProps> {
-  render() {
-    const { films } = this.props;
-
-    return (
-      <div className="card-container">
-        {films.map((film, index) => (
-          <div key={index} className="card">
-            <h2>{film.title}</h2>
-            <p>Director: {film.director}</p>
-            <p>Producer: {film.producer}</p>
-            <p>Release Date: {film.release_date}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+const FilmCards: React.FC<FilmCardsProps> = ({ films }) => {
+  return (
+    <div className="card-container">
+      {films.map((film, index) => (
+        <div key={index} className="card">
+          <h2>{film.title}</h2>
+          <p>Director: {film.director}</p>
+          <p>Producer: {film.producer}</p>
+          <p>Release Date: {film.release_date}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default FilmCards;

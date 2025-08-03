9ab674f6f9aa3a92fad602d97977a5c33c4dff54
Gulia <gulia.isaeva.pls@gmail.com>
2025-08-03 18:36:00 +0300

    feat:add completed Themecontext

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Dashboard from './components/pages/Dashboard';
 import { Flyout } from './components/Flyout';
+import { ThemeProvider } from './components/context/ThemeContext';
+import { ThemeSwitcher } from './components/ThemeSwitscher';
 
 const App: React.FC = () => {
   return (
-    <>
-      <Dashboard />;
-      <Flyout />;
-    </>
+    <ThemeProvider>
+      <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white">
+        <ThemeSwitcher />
+        <Dashboard />
+        <Flyout />
+      </div>
+    </ThemeProvider>
   );
 };
 
